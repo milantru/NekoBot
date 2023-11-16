c18e31d6b29b1457777d4269ff9da3b778fb4a36
truchanm <milantru1@gmail.com>
2023-11-16 21:30:38 +0100

    Update

--- main.py
+++
@@ -1,7 +0,0 @@
-# This file is the starting point for Replit to start the bot.
-# (But I wanted to have source codes in src folder.)
-
-import os
-
-if __name__ == "__main__":
-    os.system("./src/run_bot.py")
